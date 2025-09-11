3d1074c11e86f45746b218a37112038cb4821237
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2025-09-11 23:06:26 +0000

    Projects

--- portifolio/app/page.js
+++ portifolio/app/page.js
@@ -1,14 +1,21 @@
 import style from "./page.module.css";
 import { Apresentation } from "@/components/Apresentation";
 import { AboutMe } from "@/components/AboutMe";
+import { Projects } from "@/components/Projects";
 export default function Home() {
   return (
     <main className = {style.page}>
-      <Apresentation />
+      <div className={style.apresentation}>
+       <Apresentation />
+      </div>
       <hr></hr>
-      <AboutMe />
+      <div className={style.aboutMe}>
+        <AboutMe />
+      </div>
       <hr></hr>
-      
+      <div className={style.projects}>
+        <Projects />
+      </div>
     </main>
     
   );
