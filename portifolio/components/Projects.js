98014990c7b07b9b1815446c831049cea157abd5
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2025-09-12 12:09:14 +0000

    Card

--- portifolio/components/Projects.js
+++ portifolio/components/Projects.js
@@ -5,10 +5,12 @@ export function Projects(){
     return(
         
         <section>
-            <h1>Projetos</h1>
+            <h1 className={style.Title}>Projetos</h1>
             <div className={style.card}>
-                <h2 className={style.cardTitle}>Classificador Deadpool e Homem Aranha</h2>
+                
                 <a href="https://github.com/Williansilva2207/Rede-YOLO-para-detectar-o-Homem-Aranha-e-Deadpool"><Image src="/Dh.jpg" width={200} height={200} alt="Projeto Deadpool vs Homem Aranha" className={style.cardImage}/></a>
+                
+                <h2 className={style.cardTitle}>Classificador Deadpool e Homem Aranha</h2>
                 <p className={style.cardText}>Projeto de Machine Learning que classifica imagens do Deadpool e do Homem Aranha com rede YOLO</p>
                 <br></br>
                 
