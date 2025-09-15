b08c6c1c455d0002b412b0f2110a43f4dcbafa32
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2025-09-15 03:43:34 +0000

    Projects att

--- portifolio/components/Projects.js
+++ portifolio/components/Projects.js
@@ -4,17 +4,30 @@ import Image from "next/image";
 export function Projects(){
     return(
         
-        <section>
-            <h1 className={style.Title}>Projetos</h1>
-            <div className={style.card}>
+        
+            
+            
+        <div className={style.projects}>
+               <h1 className={style.Title}>Projetos</h1>
+               <div className={style.section}>
+                <div className={style.card}>
                 
-                <a href="https://github.com/Williansilva2207/Rede-YOLO-para-detectar-o-Homem-Aranha-e-Deadpool"><Image src="/Dh.jpg" width={200} height={200} alt="Projeto Deadpool vs Homem Aranha" className={style.cardImage}/></a>
+                    <a href="https://github.com/Williansilva2207/Rede-YOLO-para-detectar-o-Homem-Aranha-e-Deadpool"><Image src="/DS.png" width={350} height={750} alt="Projeto Deadpool vs Homem Aranha" className={style.cardImage}/></a>
                 
-                <h2 className={style.cardTitle}>Classificador Deadpool e Homem Aranha</h2>
-                <p className={style.cardText}>Projeto de Machine Learning que classifica imagens do Deadpool e do Homem Aranha com rede YOLO</p>
-                <br></br>
+                    <h2 className={style.cardTitle}>Classificador Deadpool e Homem Aranha</h2>
+                    <p className={style.cardText}>Projeto de Machine Learning que classifica imagens do Deadpool e do Homem Aranha com rede YOLO</p>
+                    
                 
-            </div>
-        </section>
+                </div>
+                <div className={style.card}>
+                
+                    <a href="https://github.com/Williansilva2207/Reducao-de-Dimensionalidade-em-Imagens-para-Redes-Neurais"><Image src="/code.png" width={350} height={750} alt="Projeto Redimensionamento de imagens" className={style.cardImage}/></a>
+                
+                    <h2 className={style.cardTitle}>Redução de Dimensionalidade de Imagem Para Redes Neurais</h2>
+                    <p className={style.cardText}>Esse projeto visa utilizar algumas bibliotecas e ferramentas pythons para a redução da Dimensionalidade de imagens, técnica essa que permite um treinamento menos custoso de redes neurais.</p>
+                </div>                
+                </div>
+        </div>
+        
     );
 }
