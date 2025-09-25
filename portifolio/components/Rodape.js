1295b50eb09f3d3b3f7758a16a0f5f77898fd232
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2025-09-25 21:13:53 +0000

    likendin link

--- portifolio/components/Rodape.js
+++ portifolio/components/Rodape.js
@@ -6,7 +6,7 @@ export function Rodape() {
     <footer className={styles.footer}>
       <ul className={styles.lista}> 
         <li><a href = "https://github.com/Williansilva2207"> <FaGithub className={styles.socialsFooter} /></a></li>
-        <li><a href = "www.linkedin.com/in/willian-rodrigues-295575297"><FaLinkedin className={styles.socialsFooter} /></a></li>
+        <li><a href = "https://www.linkedin.com/in/willian-rodrigues-295575297/"><FaLinkedin className={styles.socialsFooter} /></a></li>
         </ul>
      
       <p>Recife, PE</p>
